99c2dd47b418331c7ee1647fdf26adc3e955e985
Aleksashka301 <aleksashka301@gmail.com>
2025-05-09 18:07:18 +0500

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, Location_Image
 
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
 
+class Location_ImageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
 admin.site.register(Location, LocationAdmin)
+admin.site.register(Location_Image, Location_ImageAdmin)
