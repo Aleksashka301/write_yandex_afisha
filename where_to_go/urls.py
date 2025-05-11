3c070c3c3de5076bc88d18c5ed119c1434b539fd
Aleksashka301 <aleksashka301@gmail.com>
2025-05-11 17:25:06 +0500

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,11 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from where_to_go import views
+from places.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page),
+    path('', home_page),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
