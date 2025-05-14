ed16acbd4f72c2034d671ef747f7e30d70c2d3e0
Aleksashka301 <aleksashka301@gmail.com>
2025-05-14 17:14:02 +0500

    Loading images in location settings

--- places/admin.py
+++ places/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from .models import Location, Location_Image
 
 
+class Location_ImageInline(admin.TabularInline):
+    model = Location_Image
+
+
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',)
+    list_display = ('title',)
+    inlines = [Location_ImageInline,]
 
 
+@admin.register(Location_Image)
 class Location_ImageAdmin(admin.ModelAdmin):
     list_display = ('title', 'location',)
-
-
-admin.site.register(Location, LocationAdmin)
-admin.site.register(Location_Image, Location_ImageAdmin)
