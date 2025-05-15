0e142c44857e55bfaff83fc2eca6df73b8796e67
Aleksashka301 <aleksashka301@gmail.com>
2025-05-15 18:56:41 +0500

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from places.views import home_page, place_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     path('places/<int:place_id>/', place_detail, name='place_detail'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
