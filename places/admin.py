aeda4cce0201d801180ab273fce9e89e64b3f405
Aleksashka301 <aleksashka301@gmail.com>
2025-05-20 18:41:49 +0500

    Update admin.py

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,6 @@
 from adminsortable2.admin import SortableTabularInline, SortableAdminMixin
 from django.contrib import admin
+
 from .models import Location, LocationImage
 
 
@@ -18,3 +19,4 @@ class LocationAdmin(SortableAdminMixin, admin.ModelAdmin):
 @admin.register(LocationImage)
 class LocationImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'location',)
+    raw_id_fields = ('location', )
